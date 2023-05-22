c4e82b485d518cc1390758aeac2d423bd4a58850
hyunho4562 <kyounguk192@gmail.com>
2023-05-22 22:25:45 +0900

    2023-05-22 유저 관련 기능 테스트 작성하기

--- src/test/java/com/group/service/user/UserServiceTest.kt
+++ src/test/java/com/group/service/user/UserServiceTest.kt
@@ -1,19 +1,32 @@
 package com.group.service.user
 
+import com.group.libraryapp.domain.user.User
 import com.group.libraryapp.domain.user.UserRepository
 import com.group.libraryapp.dto.user.request.UserCreateRequest
+import com.group.libraryapp.dto.user.request.UserUpdateRequest
 import com.group.libraryapp.service.user.UserService
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.test.context.SpringBootTest
 
+@SpringBootApplication
 @SpringBootTest
 class UserServiceTest @Autowired constructor (
     private val userRepository: UserRepository,
     private val userService: UserService,
 ) {
+
+    @AfterEach
+    fun clean() {
+        userRepository.deleteAll()
+    }
+
     @Test
+    @DisplayName("유저 저장이 정상 등록한다.")
     fun saveUserTest() {
         // given
         val request = UserCreateRequest("이현호", 20)
@@ -27,5 +40,51 @@ class UserServiceTest @Autowired constructor (
         assertThat(results).hasSize(1)
         assertThat(results[0].name).isEqualTo("이현호")
         assertThat(results[0].age).isEqualTo(20)
+    }
+
+    @Test
+    @DisplayName("유저 조회가 정상 동작한다.")
+    fun getUsersTest() {
+        // given
+        userRepository.saveAll(listOf(
+            User("A", 20),
+            User("B", 19),
+        ))
+
+        // when
+        val results = userService.getUsers()
+
+        // then
+        assertThat(results).hasSize(2)
+        assertThat(results).extracting("name").containsExactlyInAnyOrder("A", "B")
+        assertThat(results).extracting("age").containsExactlyInAnyOrder(20, 19)
+    }
+
+    @Test
+    @DisplayName("유저 업데이트가 정상 동작한다.")
+    fun updateUserNameTest() {
+        // given
+        val savedUser = userRepository.save(User("A", 20))
+        val request = UserUpdateRequest(savedUser.id, "B")
+
+        // when
+        userService.updateUserName(request)
+
+        // then
+        val result = userRepository.findAll()[0]
+        assertThat(result.name).isEqualTo("B")
+    }
+
+    @Test
+    @DisplayName("유저 삭제가 정상 동작한다.")
+    fun deleteUserNameTest() {
+        // given
+        userRepository.save(User("A", 20))
+
+        // when
+        userService.deleteUser("A")
+
+        // then
+        assertThat(userRepository.findAll()).isEmpty()
     }
 }
